a24edf532375ab321357de470f7d37c525fcd414
Petrav <vagi.p94@gmail.com>
2019-05-15 13:06:50 +0200

    FlyingEnemy refactored

--- src/com/codecool/snake/entities/enemies/FlyingEnemy.java
+++ src/com/codecool/snake/entities/enemies/FlyingEnemy.java
@@ -1,20 +1,13 @@
 package com.codecool.snake.entities.enemies;
 
-import com.codecool.snake.Globals;
 import com.codecool.snake.Utils;
 import com.codecool.snake.entities.Animatable;
-import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.entities.Interactable;
-import com.codecool.snake.entities.snakes.SnakeHead;
 import javafx.geometry.Point2D;
-
-import java.util.Random;
 
 
 public class FlyingEnemy extends EnemyBasic implements Animatable, Interactable {
 
-    private Point2D heading;
-    private static Random rnd = new Random();
     private double direction = 0;
     private int speed = Utils.generateRandomBetween(1, 6);
 
@@ -31,7 +24,7 @@ public class FlyingEnemy extends EnemyBasic implements Animatable, Interactable 
         changeDirection();
         setRotate(direction);
 
-        heading = Utils.directionToVector(direction, speed);
+        Point2D heading = Utils.directionToVector(direction, speed);
 
         if (isOutOfBounds()) {
             destroy();
